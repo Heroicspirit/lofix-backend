49eb20e9fe5c632dd9088c5d740d13b81b9cb640
Heroicspirit <astarana21@gmail.com>
2026-01-17 16:06:19 +0545

    done

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -4,6 +4,7 @@ export const UserSchema = z.object({
     name:z.string().min(3),
     email: z.string().min(6),
     password:z.string().min(6),
+    profilePicture: z.string().url("Invalid URL format").optional(),
     role: z.enum(['admin','user']).default('user'),
 });
 export type UserType = z.infer<typeof UserSchema>;
